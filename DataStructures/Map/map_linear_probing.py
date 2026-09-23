dfc81742c4e42dfc2dde5194748e06ffe2aef022
Jorge <j.claross2@uniandes.edu.co>
2026-09-23 14:41:33 -0500

    funciones 2

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -22,6 +22,58 @@ def find_slot(my_map, key, hash_value):
       hash_value = (hash_value + 1) % my_map["capacity"]
    return ocupied, first_avail
 
+def get(my_map, key):
+   """
+   Retorna el valor asociado a la llave ``key`` en el mapa.
 
+   :param my_map: Mapa del cual se desea obtener el valor.
+   :type my_map: :ref:`map<map>`
+   :param key: Llave de la cual se desea obtener el valor.
+   :type key: any
 
+   :return: Valor asociado a la llave ``key`` en el mapa.
+   :rtype: any
+   """
+   hash_value = mf.hash_value(my_map, key)
+   ocupied, slot = find_slot(my_map, key, hash_value)
+   if ocupied:
+      entry = lt.get_element(my_map["table"], slot)
+      return me.get_value(entry)
+   else:
+      return None
+
+def remove(my_map, key):
+   """
+   Elimina la entrada asociada a la llave ``key`` en el mapa.
+
+   :param my_map: Mapa del cual se desea eliminar la entrada.
+   :type my_map: :ref:`map<map>`
+   :param key: Llave de la entrada que se desea eliminar.
+   :type key: any
+
+   :return: Valor asociado a la llave ``key`` en el mapa antes de ser eliminada.
+   :rtype: any
+   """
+   hash_value = mf.hash_value(my_map, key)
+   ocupied, slot = find_slot(my_map, key, hash_value)
+   if ocupied:
+      entry = lt.get_element(my_map["table"], slot)
+      value = me.get_value(entry)
+      me.set_key(entry, None)
+      me.set_value(entry, None)
+      return value
+   else:
+      return None
+  
+def size(my_map):
+   """
+   Retorna el número de entradas en el mapa.
+
+   :param my_map: Mapa del cual se desea obtener el tamaño.
+   :type my_map: :ref:`map<map>`
+
+   :return: Número de entradas en el mapa.
+   :rtype: int
+   """
+   return my_map["size"]
 
